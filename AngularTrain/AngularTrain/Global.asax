8d89108a19e8d2a21b231014ac7e6d4ac233121c
teeraboon <teeraboon@longkongstudio.com>
2013-07-30 09:09:51 +0700

    first

---
+++ AngularTrain/AngularTrain/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AngularTrain.WebApiApplication" Language="C#" %>
---
+++ AngularTrain/AngularTrain/Scripts/Js/scope.js
@@ -0,0 +1,27 @@
+﻿function LeftCtrl($scope) {
+    $scope.scname = "LeftCtrl";
+}
+
+function RightCtrl($scope) {
+    $scope.scname = "RightCtrl";
+
+}
+
+function MidCtrl($scope) {
+    $scope.scname = "MidCtrl";
+    $scope.midonly = "MidOnly";
+    
+
+}
+
+function C11Ctrl($scope) {
+    $scope.scname = "C11Ctrl";
+    $scope.C11only = "C11Only";
+   
+}
+
+
+function C12Ctrl($scope) {
+    $scope.scname = "C12Ctrl";
+
+}
